1ca0a3566d057e294016d8ec153c81e38e1bb79e
Benjamin Gregory <benjaminrgregory@gmail.com>
2018-01-25 10:47:00 -0600

    update schemas

---
+++ schemas/_schema.py
@@ -0,0 +1,12 @@
+from MarketoPlugin.schemas.activities import activities
+from MarketoPlugin.schemas.campaigns import campaigns
+from MarketoPlugin.schemas.lead_lists import lead_lists
+from MarketoPlugin.schemas.leads import leads
+from MarketoPlugin.schemas.programs import programs
+
+
+schema = {'activities': activities,
+          'campaigns': campaigns,
+          'lead_lists': lead_lists,
+          'leads': leads,
+          'programs': programs}
